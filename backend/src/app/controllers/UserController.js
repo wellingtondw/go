f76bacec782713c7c63cbaccc78fe0770ce14f05
wellington <wellingtonslpbi@hotmail.com>
2020-04-03 20:52:32 -0300

    create auth middleware

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -17,6 +17,13 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    res.json({
+      success: true,
+    });
+  }
 }
 
 export default new UserController();
