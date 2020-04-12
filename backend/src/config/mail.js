7c43c12ceb8cbbb9c9c4dbac6ecb4c9b3e24d0ac
wellington <wellingtonslpbi@hotmail.com>
2020-04-12 20:30:07 -0300

    config nodemail and send mail

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "27baefe2ac0d5f",
+    pass: "cd6af6e52e387e",
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com",
+  },
+};
