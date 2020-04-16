b90a2e9fb2a80e424260edee900f32dc31e31326
wellington <wellingtonslpbi@hotmail.com>
2020-04-16 20:01:01 -0300

    create global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 import './config/ReactotronConfig';
 
 function App() {
 	return (
 		<Router history={history}>
 			<Routes />
+			<GlobalStyle />
 		</Router>
 	);
 }
